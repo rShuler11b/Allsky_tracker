2f22aa5cb1b544f90aba29033d6244417a2128e2
shuler11b <ryan.shuler11b@gmail.com>
2024-10-31 19:07:01 -0700

    Update Allsky_Main_v2.py

--- Allsky_Main_v2.py
+++ Allsky_Main_v2.py
@@ -1,6 +1,6 @@
 from db_utils import init_db
 from menu import display_menu
-from server import start_flask_server
+from Flask_server import start_flask_server
 import cv2
 
 if __name__ == '__main__':
